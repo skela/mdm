802ab4ef5044390064b75eca0b8f51ccf514e0d3
Alek Slater <skela@davincium.com>
2025-11-23 15:56:50 +0100

    add default bg image, using an endeavour os from the community bgs for now
    plan is to replace it with a proper teknolab bg

--- configure.py
+++ configure.py
@@ -24,3 +24,7 @@ packages = [
 
 for package in packages:
 	install(package)
+
+bg = "/home/teknolab/.mdm/res/bg.jpg"
+execute(f"gsettings set org.gnome.desktop.background picture-uri {bg} ")
+execute(f"gsettings set org.gnome.desktop.background picture-uri-dark {bg} ")
